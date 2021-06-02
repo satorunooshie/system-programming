7f1554da607f00aa1c16c8931ef7274b5f3f0349
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-03 04:01:07 +0900

    PNGファイル分析してみた

--- read.go
+++ read.go
@@ -96,4 +96,49 @@ func main() {
 		return
 	}
 	fmt.Printf("data: %d\n", i)
+
+	png, err := os.Open("xxx.png")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := png.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	chunks := readChunks(png)
+	for _, chunk := range chunks {
+		dumpChunk(chunk)
+	}
+}
+
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	if err := binary.Read(chunk, binary.BigEndian, &length); err != nil {
+		return
+	}
+	buffer := make([]byte, 4)
+	if _, err := chunk.Read(buffer); err != nil {
+		return
+	}
+	fmt.Printf("chunk '%v', (%d bytes)\n", string(buffer), length)
+}
+
+func readChunks(file *os.File) []io.Reader {
+	var chunks []io.Reader
+	if _, err := file.Seek(8, 0); err != nil {
+		return chunks
+	}
+	var offset int64 = 8
+	for {
+		var length int32
+		if err := binary.Read(file, binary.BigEndian, &length); err == io.EOF {
+			break
+		}
+		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
+		// 次のチャンクの先頭に移動
+		// 現在位置は長さを読み終わった箇所なので、チャンク名(4バイト)+データ長+CRC(4バイト)先に移動
+		offset, _ = file.Seek(int64(length+8), 1)
+	}
+	return chunks
 }
