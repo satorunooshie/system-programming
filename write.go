c3f1e5e167d59997c76168f32a4936c761576281
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-02 20:15:09 +0900

    インターネットアクセスの送信

--- write.go
+++ write.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -24,4 +26,26 @@ func main() {
 	var builder strings.Builder
 	builder.Write([]byte("strings.Builder example\n"))
 	fmt.Println(builder.String())
+
+	// net.Conn(通信のコネクションを表すinterface)はio.Reader/io.Writer
+	// interfaceの実態はnet.TCPConn構造体のポインタ
+	conn, err := net.Dial("tcp", "ascii.jp:80")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := io.WriteString(conn, "GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"); err != nil {
+		return
+	}
+	// net.Connはio.Reader interface
+	if _, err := io.Copy(os.Stdout, conn); err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("GET", "http://ascii.jp", nil)
+	if err != nil {
+		panic(err)
+	}
+	if err := req.Write(conn); err != nil {
+		return
+	}
 }
