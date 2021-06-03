b0ee25f1eca30da56b02b5041e850ec3ee75ce80
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-04 00:31:43 +0900

    チャネルとselect文

--- channel.go
+++ channel.go
@@ -21,6 +21,26 @@ func main() {
 	for n := range pn {
 		fmt.Println(n)
 	}
+
+	// ブロックする複数のチャネルを同時に並列で待ち受け、データが到達したチャネルから順に取り出して処理する、あるいはブロックする複数のチャネルの書き込みが完了するのを並列で待ち受け、データが先に送信できたチャネルにのみデータを投入するにはselect文を使う
+	// selectは一度トリガーすると終わってしまうため、forループに入れて使われることが多い
+	// selectを使って複数のチャネルへの書き込みのブロッキングを扱うこともできる
+	// case tasks <- "make clean":
+	/*
+		for {
+			select {
+			case data := <-reader:
+				// 読み込んだデータを利用
+			case <-exit:
+				// ループを抜ける
+				break
+			// この場合はブロックせずにすぐ終了するのでチャネルにデータが入るまでポーリングでループを回したい時に使える
+			default:
+				// まだデータが来ていない
+				break
+			}
+		}
+	*/
 }
 
 func sub() {
@@ -37,7 +57,7 @@ func primeNumber() (result chan int) {
 			l := int(math.Sqrt(float64(i)))
 			hasFound := false
 			for j := 3; j < l; j += 2 {
-				if i % j == 0 {
+				if i%j == 0 {
 					hasFound = true
 					break
 				}
