027ecc140fb5c2ed59228118fecd8c2f7229988c
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-06 20:58:03 +0900

    データグラム型のUnixドメインソケット

--- unix.go
+++ unix.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"net/http"
-	"net/http/httputil"
 	"os"
 	"path/filepath"
 )
@@ -85,31 +82,94 @@ func main() {
 													   }
 			}()
 		}
-	 */
+	*/
 
 	// Unixドメインソケット版のHTTPクライアント
-	conn, err := net.Dial("unix", filepath.Join(os.TempDir(), "unixdomainsocket-sample"))
+	/*
+		conn, err := net.Dial("unix", filepath.Join(os.TempDir(), "unixdomainsocket-sample"))
+		if err != nil {
+			panic(err)
+		}
+		request, err := http.NewRequest(
+			"GET",
+			"http://localhost:8888",
+			nil,
+		)
+		if err != nil {
+			panic(err)
+		}
+		if err := request.Write(conn); err != nil {
+			panic(err)
+		}
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			panic(err)
+		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+	*/
+
+	// データグラム型のUnixドメインソケット(サーバー)
+	/*
+		path := filepath.Join(os.TempDir(), "unixdomainsocket-server")
+		_ = os.Remove(path)
+		fmt.Println("Server is running at " + path)
+		conn, err := net.ListenPacket("unixgram", path)
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := conn.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		buffer := make([]byte, 1500)
+		for {
+			length, remoteAddress, err := conn.ReadFrom(buffer)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Received from %v: %v\n", remoteAddress, string(buffer[:length]))
+			if _, err := conn.WriteTo([]byte("Hello from Server"), remoteAddress); err != nil {
+				panic(err)
+			}
+		}
+	*/
+
+	// クライアント
+	// net.Dial()の引数を変更するだけではError
+	// サーバー側のconn.ReadFrom()呼び出しで取得できるアドレスがnilになってしまうため。net.Dial()で開いたソケットは一方的な送信用でアドレスと結び付けられていないので。
+	// 解決方法はクライアント側もサーバー側と同じく、初期化を行い、net.PacketConnインターフェースのWriteTo()メソッド、ReadFrom()メソッドを使って送受信する
+	// 送信を自分の受信用のソケットファイルを持っているソケットから実行すれば、サーバーのReadFrom()で返信可能なアドレスが得られる
+	clientPath := filepath.Join(os.TempDir(), "unixdomainsocket-client")
+	_ = os.Remove(clientPath)
+	conn, err := net.ListenPacket("unixgram", clientPath)
 	if err != nil {
 		panic(err)
 	}
-	request, err := http.NewRequest(
-		"GET",
-		"http://localhost:8888",
-		nil,
-	)
+	// 送信先のアドレス
+	unixServerAddr, err := net.ResolveUnixAddr("unixgram", filepath.Join(os.TempDir(), "unixdomainsocket-server"))
 	if err != nil {
 		panic(err)
 	}
-	if err := request.Write(conn); err != nil {
+	var serverAddr net.Addr = unixServerAddr
+	defer func() {
+		if err := conn.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	fmt.Println("Sending to Server")
+	if _, err := conn.WriteTo([]byte("Hello from Client"), serverAddr); err != nil {
 		panic(err)
 	}
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+	fmt.Println("Receiving from Server")
+	buffer := make([]byte, 1500)
+	length, _, err := conn.ReadFrom(buffer)
 	if err != nil {
 		panic(err)
 	}
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
 }
